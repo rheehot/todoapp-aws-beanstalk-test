83865f86da577dd7de576a0c75dade0abc68430f
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2021-08-29 19:37:34 +0900

    Commit before yarn eject

---
+++ 3-styling-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
